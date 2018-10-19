29b1a20add4325ca4441abf84ce7f39d47f85e4e
Unknown <bapt.papa@gmail.com>
2018-10-19 11:47:01 +0200

    je met mon projet

---
+++ CLaGuerre/Trebuchet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLaGuerre
+{
+    class Trebuchet : ArmeDeJet
+    {
+        public Trebuchais(int Pdv)
+        {
+            base.Pdv = Pdv;
+            Console.WriteLine(base.Pdv);
+        }
+    }
+}
